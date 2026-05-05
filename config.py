df4e0a853f94b36950588257801d1dfe9aef5e40
hadiamoosa40 <hadiamoosa40@gmail.com>
2026-05-05 21:30:37 +0500

    upd

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
 GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
 GITHUB_REDIRECT_URI = os.getenv("GITHUB_REDIRECT_URI")
 
-GITHUB_WEBHOOK_SECRET = os.getenv("GITHUB_WEBHOOK_SECRET")
-
 JWT_SECRET = os.getenv("JWT_SECRET")
-MONGODB_URI = os.getenv("MONGODB_URI")
+MONGO_URI = os.getenv("MONGO_URI")
+
+GITHUB_WEBHOOK_SECRET = os.getenv("GITHUB_WEBHOOK_SECRET")
+FRONTEND_URL = os.getenv("FRONTEND_URL")  # e.g. http://localhost:5173
