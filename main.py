f0ccf9d2985052f743335414e018ea9498926eaf
hadiamoosa40 <hadiamoosa40@gmail.com>
2026-05-06 11:11:08 +0500

    main upd

--- main.py
+++ main.py
@@ -51,5 +51,5 @@ async def health():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
