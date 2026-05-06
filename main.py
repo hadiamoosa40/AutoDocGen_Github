8cca336d9b5523edc9493a81a0df8aaee7161b32
hadiamoosa40 <hadiamoosa40@gmail.com>
2026-05-07 00:21:21 +0500

    update

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
-from routes import auth, github, webhook
 from fastapi.middleware.cors import CORSMiddleware
+from routes import auth, github, webhook
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict this
+    allow_origins=[
+        "http://localhost:5173",   # local frontend
+        "https://autodocgengithub-production.up.railway.app"  # optional (same backend)
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(auth.router, prefix="/auth")
 app.include_router(github.router, prefix="/github")
 app.include_router(webhook.router)
