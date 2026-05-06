322bdc1fd8c30cf6673717f59f827d780ae112ed
hadiamoosa40 <hadiamoosa40@gmail.com>
2026-05-07 00:13:50 +0500

    updated

--- routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import router as auth_router
-from .github import router as github_router
-from .webhook import router as webhook_router
-from .websocket import router as websocket_router
-
-__all__ = ["auth_router", "github_router", "webhook_router", "websocket_router"]
