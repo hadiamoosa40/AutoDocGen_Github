ada1ed6c2e041c3ce57e89849d87157e55da01a6
hadiamoosa40 <hadiamoosa40@gmail.com>
2026-05-07 00:56:18 +0500

    update

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",   # local frontend
-        "https://autodocgengithub-production.up.railway.app"  # optional (same backend)
+        "http://localhost:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
